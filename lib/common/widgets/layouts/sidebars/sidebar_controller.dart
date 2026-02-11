e5d31810e1aedf7108cefd471c6f7a5e48b46e3b
rahulkashyap7 <rahulkashyap97593@gmail.com>
2026-02-11 19:51:30 +0530

    Breadcrumbs added

--- lib/common/widgets/layouts/sidebars/sidebar_controller.dart
+++ lib/common/widgets/layouts/sidebars/sidebar_controller.dart
@@ -2,25 +2,24 @@ import 'package:ecommerce_admin_panel/routes/routes.dart';
 import 'package:ecommerce_admin_panel/utils/device/device_utility.dart';
 import 'package:get_x/get.dart';
 
-class SidebarController extends GetxController{
-
+class SidebarController extends GetxController {
   final activeItem = RRoutes.dashboard.obs;
   final hoverItem = ''.obs;
 
-  void changeActiveItem(String route) => activeItem.value;
+  void changeActiveItem(String route) => activeItem.value = route;
 
   void changeHoverItem(String route) {
-    if(!isActive(route)) hoverItem.value = route;
+    if (!isActive(route)) hoverItem.value = route;
   }
 
   bool isActive(String route) => activeItem.value == route;
   bool isHovering(String route) => hoverItem.value == route;
 
-  void menuOnTap(String route){
-    if(isActive(route)){
+  void menuOnTap(String route) {
+    if (!isActive(route)) {
       changeActiveItem(route);
 
-      if(RDeviceUtils.isMobileScreen(Get.context!)) Get.back();
+      if (RDeviceUtils.isMobileScreen(Get.context!)) Get.back();
 
       Get.toNamed(route);
     }
